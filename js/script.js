6019116d665c93069e56ec5382439c93229f675d
Büşra Özata <busraozata@Busra-MacBook-Air.local>
2022-04-22 06:47:48 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 1,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: '.swiper-pagination',
+      clickable: true,
     },
+  });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
     },
 });
